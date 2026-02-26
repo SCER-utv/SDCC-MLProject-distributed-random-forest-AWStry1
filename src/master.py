dacd433111820b8f90d825ab3555d1485542ae5d
Emanuele Russo <150815706+emarussoo@users.noreply.github.com>
2026-02-26 18:42:04 +0100

    Update master.py

--- src/master.py
+++ src/master.py
@@ -152,9 +152,6 @@ def process_training_job(dataset, workers_list, trees, strategy_file='config/wor
     print(f"   - Sorgente Train: {train_s3_uri}")
     print(f"   - Sorgente Test:  {test_s3_uri}")
 
-    print("napoli")
-    print("bari")
-
     # 2. Setup Master (Iniettiamo la strategia nel costruttore)
     grpc_master = GrpcMaster(config, strategy)
     grpc_master.connect()
