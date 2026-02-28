0a407eaaaeec0827827a2e77b2d5a21ffa6bbd05
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2026-02-28 17:16:38 +0100

    Add files via upload

---
+++ src/core/data_managers/airlines_data_manager.py
@@ -0,0 +1,6 @@
+import os
+from src.core.data_managers.data_managers import BaseDataManager
+
+class AirlinesDataManager(BaseDataManager):
+    def get_target_column(self):
+        return 'TARGET_DELAY'
