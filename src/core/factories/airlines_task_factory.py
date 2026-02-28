ecd3f516c58b88cfb8509e8838688b821ddeb0ef
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2026-02-28 17:50:48 +0100

    Update airlines_task_factory.py

--- src/core/factories/airlines_task_factory.py
+++ src/core/factories/airlines_task_factory.py
@@ -1,4 +1,4 @@
-from src.core.data_managers.airlines_data_manager
+from src.core.data_managers.airlines_data_manager import AirlinesDataManager
 from src.core.factories.factories import TaskFactory
 from src.core.ml_strategies.classification_ml_strategy import ClassificationMLStrategy
 from src.core.strategies.classification_strategy import ClassificationStrategy
@@ -7,3 +7,4 @@ class AirlinesTaskFactory(TaskFactory):
     def create_strategy(self): return ClassificationStrategy()
     def create_data_manager(self, strategy): return AirlinesDataManager(strategy)
     def create_ml_strategy(self): return ClassificationMLStrategy()
+
